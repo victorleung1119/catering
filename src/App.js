e6b36ae33e679dfb3da0b8126a1d12c7357642bc
Victor Leung <victorleung1119@gmail.com>
2018-04-29 16:16:33 -0500

    Add contact page and connect to router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './routes/Home/components/Home/Home'
 import About from './routes/About/components/About/About'
+import Contact from './routes/Contact/components/Contact/Contact'
 import Header from './components/Header/Header'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
@@ -15,6 +16,7 @@ class App extends Component {
             <Header />
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
           </div>
         </Router>
       </div>
