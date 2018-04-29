765e0d332d582c259eaffc8bbfd58d8cd912ceda
Victor Leung <victorleung1119@gmail.com>
2018-04-29 15:19:19 -0500

    Move styles from css to js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ class App extends Component {
         <div className="bgwidget">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-12 col-sm-6 d-flex align-items-center justify-content-center insidewidget">
+              <div className="col-12 col-sm-6 d-flex align-items-center justify-content-center" style={{
+                height: `500px`,
+              }}>
                 <div className="d-flex flex-column align-items-center justify-content-center">
                   <h3 className="align-items-center text-center text-white">荔蓉香酥帶子</h3>
                   <button type="button" className="btn btn-danger mt-3">CATER NOW</button>
@@ -40,11 +42,13 @@ class App extends Component {
           </div>
         </div>
         <div className="container-fluid py-1">
-          <div className="row">
+          <div className="row" style={{
+            height: `300px`,
+          }}>
             <div className="col-12 col-sm-4 p-1 d-flex align-items-center justify-content-center lilbox img-fluid ourFood text-light">
               <h3>OUR FOOD</h3>
             </div>
-            <div className="col-12 col-sm-4 p-1 d-flex align-items-center justify-content-center lilbox img-fluid ourStory">
+            <div className="col-12 col-sm-4 p-1 d-flex align-items-center justify-content-center lilbox img-fluid ourStory text-light">
               <h3>OUR STORY</h3>
             </div>
             <div className="col-12 col-sm-4 p-1 d-flex align-items-center justify-content-center lilbox img-fluid contactUs">
